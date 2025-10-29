3f90a2a5c8e6ca7ea312a022c4c869c839d1ecb3
gonzalogtzr <tiano_gonz@hotmail.com>
2025-10-29 15:51:57 -0600

    Add Threads

---
+++ main_thread.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+def blink_led():
+    while True:
+        print("Hilo 1..")
+        time.sleep(1)
+
+def control_servo():
+    while True:
+        print("Hilo 2..")
+        time.sleep(2)
+
+# Create thread for LED
+led_thread = threading.Thread(target=blink_led)
+led_thread.daemon = True # Allows the main program to exit even if this thread is still running
+
+# Create thread for servo
+servo_thread = threading.Thread(target=control_servo)
+servo_thread.daemon = True
+
+# Start the threads
+led_thread.start()
+servo_thread.start()
+
+# Main program continues its own tasks
+while True:
+    print("Main program running...")
+    time.sleep(3)
